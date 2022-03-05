fa3cce8f235f3a329f0adedd76ce77352408968a
naveenreddiedodla <99010716+naveenreddiedodla@users.noreply.github.com>
2022-03-05 12:38:47 +0530

    Create 3_act.h

---
+++ 3_Implementation/Inc/3_act.h
@@ -0,0 +1,28 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+#define SET_TIMER1A_FAST_PWM TCCR1A|=(1<<COM1A1)|(1<<WGM10)|(1<<WGM11)
+
+/**
+ * @brief A macro to prescale timer1 for 64 and wave generation mode for 10bit digital input
+ * 
+ */
+#define PRESCALE_TIMER_64 TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10)
+
+
+#define SET_PWM_AS_OUTPUT DDRB|=(1<<PB1)
+
+/**
+ * @brief A function to setup timer1 with channel A for pulse width modulation with wave generation mode of 10 bits fast PWM and prescaling of 64.
+ * 
+ */
+void TimerWaveGenMode(void);
+
+ 
+
+int outputbyPWM(uint16_t ADC_val);
+
+#endif
