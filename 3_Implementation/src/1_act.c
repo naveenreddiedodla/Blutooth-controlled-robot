c80730c9c6c32851a5376b413a68edd40d50d8e6
naveenreddiedodla <99010716+naveenreddiedodla@users.noreply.github.com>
2022-03-05 12:41:13 +0530

    Create 1_act.c

---
+++ 3_Implementation/src/1_act.c
@@ -0,0 +1,15 @@
+#include "1_act.h"
+
+/**
+ * @brief function for declaring peripherals led,button,heater.
+ * 
+ */
+void peripheral_init(){
+SET_LED_AS_OUTPUT;
+ SET_BUTTON_AS_INPUT;
+ SET_HEATER_AS_INPUT;
+ PULL_UP_BUTTON;
+ PULL_UP_HEATER ;
+
+
+}
